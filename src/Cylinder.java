021af0c7108ad832a4576ce950e1e6cc1128414f
ardatechie <ardatallinn@gmail.com>
2025-04-10 18:31:32 +0200

    Little challenges for my B12.

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,4 +1,26 @@
 package src;
 
-public class Cylinder {
+public class Cylinder extends Circle {
+    private double height;
+
+    // Constructor with parameters for both radius and height
+    public Cylinder(double radius, double height) {
+        super(radius);  // Call the parent constructor for radius
+        if (height <= 0) {  // Check if the height is invalid (<= 0)
+            this.height = 0.0;  // Set height to 0.0 if invalid
+        } else {
+            this.height = height;  // Set height to the provided value if valid
+        }
+    }
+
+    // Method to get the height of the cylinder
+    public double getHeight() {
+        return this.height;
+    }
+
+    // Method to calculate and return the volume of the cylinder
+    public double getVolume() {
+        return getArea() * height;  // Use getArea() from Circle class to calculate volume
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+        Circle circle = new Circle(3.5);
+        System.out.println("Circle radius: " + circle.getRadius());
+        System.out.println("Circle area: " + circle.getArea());
 
-
+        Cylinder cylinder = new Cylinder(3.5, 5.0);
+        System.out.println("Cylinder radius: " + cylinder.getRadius());
+        System.out.println("Cylinder height: " + cylinder.getHeight());
+        System.out.println("Cylinder volume: " + cylinder.getVolume());
     }
 }
